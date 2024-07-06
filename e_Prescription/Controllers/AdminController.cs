84d4feb275ceccd433e7ef87f0a38a70a75b5980
Simbongile Zameko <s224432788@mandela.ac.za>
2024-07-07 00:18:19 +0200

    Users count

--- e_Prescription/Controllers/AdminController.cs
+++ e_Prescription/Controllers/AdminController.cs
@@ -1,16 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using e_Prescription.Data;
+using Microsoft.AspNetCore.Mvc;
+using e_Prescription.Models.Account;
 
 namespace e_Prescription.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Users()
         {
-            return View();
+            var ActiveNurseCount = _context.Users.Count(a => a.Role == "Nurse" && a.IsActive);
+            var NonActiveNurseCount = _context.Users.Count(a => a.Role == "Nurse" && a.IsActive == false);
+
+            var ActiveSurgeonCount = _context.Users.Count(a => a.Role == "Surgeon" && a.IsActive);
+            var NonActiveSurgeonCount = _context.Users.Count(a => a.Role == "Surgeon" && a.IsActive == false);
+
+            var ActivePharmacistCount = _context.Users.Count(a => a.Role == "Pharmacist" && a.IsActive);
+            var NonActivePharmacistCount = _context.Users.Count(a => a.Role == "Pharmacist" && a.IsActive == false);
+
+            var ActiveAdminCount = _context.Users.Count(a => a.Role == "Admin" && a.IsActive);
+            var NonActiveAdminCount = _context.Users.Count(a => a.Role == "Admin" && a.IsActive == false);
+
+            var model = new UserCountViewModel
+            {
+                NurseCount = ActiveNurseCount,
+                NoNurseCount = NonActiveNurseCount,
+
+                SurgeonCount = ActiveSurgeonCount,
+                NoSurgeonCount = NonActiveSurgeonCount,
+
+                PharmacistCount = ActivePharmacistCount,
+                NoPharmacistCount = NonActivePharmacistCount,
+
+                AdminCount = ActiveAdminCount,
+                NoAdminCount = NonActiveAdminCount,
+            };
+
+            return View(model);
         }
     }
 }
---
+++ e_Prescription/Models/Account/UserCountViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace e_Prescription.Models.Account
+{
+    public class UserCountViewModel
+    {
+        public int NurseCount { get; set; }
+
+        public int SurgeonCount { get; set; }
+
+        public int PharmacistCount { get; set; }
+
+        public int AdminCount { get; set; }
+
+        //Not Active User Count
+        public int NoNurseCount { get; set; }
+
+        public int NoSurgeonCount { get; set; }
+
+        public int NoPharmacistCount { get; set; }
+
+        public int NoAdminCount { get; set; }
+    }
+}
