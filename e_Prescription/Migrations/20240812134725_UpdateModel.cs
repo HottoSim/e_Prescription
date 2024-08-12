f9a61fc23bad24914cf19d9af3a75c200d6ea270
Simbongile Zameko <s224432788@mandela.ac.za>
2024-08-12 21:31:21 +0200

    Med model

---
+++ e_Prescription/Migrations/20240812134725_UpdateModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_Prescription.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicationId",
+                table: "PatientMedications",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientMedications_MedicationId",
+                table: "PatientMedications",
+                column: "MedicationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PatientMedications_Medications_MedicationId",
+                table: "PatientMedications",
+                column: "MedicationId",
+                principalTable: "Medications",
+                principalColumn: "MedicationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PatientMedications_Medications_MedicationId",
+                table: "PatientMedications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PatientMedications_MedicationId",
+                table: "PatientMedications");
+
+            migrationBuilder.DropColumn(
+                name: "MedicationId",
+                table: "PatientMedications");
+        }
+    }
+}
